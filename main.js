49d54a8087333b9edd0bfb1a7686d2e925e35e44
Han <seungyoonee@gmail.com>
2022-07-07 22:39:40 -0400

    7/7

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const toggleBtn = document.querySelector('.navbar__toggleBtn');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
+const toggleBtn = document.querySelector('.navbar-toggleBtn');
+const menu = document.querySelector('.navbar-menu');
+const icons = document.querySelector('.navbar-icons');
 
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
@@ -8,6 +8,7 @@ toggleBtn.addEventListener('click', () => {
 });
 
 // jQuery to collapse the navbar on scroll
+
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
